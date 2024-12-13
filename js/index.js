17aafd08d5fb2f9b7b5daff4f2fba6de1802da85
Daniel Pelstring <danielpelstring@Daniels-MacBook-Pro.local>
2024-12-13 13:04:27 -0500

    added some variables to index.js

--- js/index.js
+++ js/index.js
@@ -1,3 +1,20 @@
 // Prevent us from attempting to use variables
 // that are not declared
 "use strict"
+
+/*const kidone = "Hayley";
+console.log(kidone);
+
+const kidtwo = "Clare";
+console.log(kidtwo);
+
+let pet = "Loca";
+console.log(pet);
+
+pet = "Snoopy";
+console.log(pet);
+pet = "Loca & Snoopy";
+console.log(pet);
+
+let favfood = "Gyro";
+console.log(favfood);*/
